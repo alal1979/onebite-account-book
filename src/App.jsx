d837d5aaa526f610cb4ab0999cf69d4f8c0510db
hyungyu1980 <lele2007@naver.com>
2026-01-06 00:50:09 +0900

    Day19 미션

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,107 @@
  import Home from './pages/Home';
  import EditTransaction from './pages/EditTransaction';
  import NewTransaction from './pages/NewTransaction';
+import { createContext, useReducer ,useRef } from 'react';
+
+const mockData = [
+  {
+    id: 0,
+    name: "마라탕 & 꿔바로우",
+    amount: 59000,
+    type: "expense",
+    category: "🍚 식비",
+    date: new Date().getTime() + 1,
+  },
+  {
+    id: 1,
+    name: "월세",
+    amount: 500000,
+    type: "expense",
+    category: "🏠 생활",
+    date: new Date().getTime() + 2,
+  },
+  {
+    id: 2,
+    name: "월급",
+    amount: 3500000,
+    type: "income",
+    category: "🏢 급여",
+    date: new Date().getTime() + 3,
+  },
+];
+
+function reducer(state,action){ 
+    switch(action.type){
+      case 'INIT':
+        return state;
+      case 'CREATE':
+        return [action.data,  ...state]; 
+      case 'UPDATE':
+        return state.map((item)=>  String(item.id) ===  String(action.data.id) ? action.data : item );
+      case 'DELETE':
+         return state.filter((item)=> String(item.id) !== String(action.id) );
+      default:
+        return state;
+   
+    }
+}
+
+export const TransactionStateContext  = createContext();
+export const TransactionDispatchContext  = createContext();
 
 function App() {
-  
+  const [transactions, setTransactions] = useReducer(reducer,mockData);
+  const idRef = useRef(3);
+
+  const onCreateTransaction = (name, amount,type , category,date) =>{
+    dispatch({
+      type:"CREATE",
+      data:{
+        id : idRef.current++,
+        name,
+        amount,
+        type,
+        category,
+        date,
+      },
+    });
+  }
+
+
+  const onUpdateTransaction = (id, name,amount,type,category,date)=>{
+    dispatch({
+      type:"UPDATE",
+      data:{
+        id,
+        name,
+        amount ,
+        type,
+        category,
+        date,
+      },
+    })
+  }
+
+  const onDeleteTransaction =(id)=>{
+    ditpatch({
+      type:"DELETE",
+      id,
+    });
+  }
+
 
   return (
+    <TransactionStateContext.Provider value={transactions}>
+      <TransactionDispatchContext.Provider value={{
+        onCreateTransaction, onUpdateTransaction ,onDeleteTransaction
+      }}>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/new-transaction" element={<NewTransaction/>} /> 
       <Route path="/edit-transaction/:id" element={<EditTransaction/>} /> 
     </Routes>
+    </TransactionDispatchContext.Provider>
+    </TransactionStateContext.Provider>
   )
 }
 
